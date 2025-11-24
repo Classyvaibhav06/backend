b1db7f12bb7830a5806cc6bd576ea2c991a5fb24
vaibhav <vaibhav7290119@gmail.com>
2025-11-25 02:44:26 +0530

    made basic ui using ejs

--- noteweb/index.js
+++ noteweb/index.js
@@ -1,11 +1,17 @@
 const express =require('express')
 let app = express();
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get("/",(req,res)=>{
-  res.send("runnnin fine")
+  res.render("index")
+})
+app.get("/about/:user",(req,res)=>{
+  res.send(`hello,${req.params.user}!`)
 })
 
 app.listen(3000,()=>{
